fbc727d985f1705155adb1f46dfa45fb3c9042fe
Krisztian Balla <kriszi.balla@gmail.com>
2025-09-08 21:27:29 +0100

    chore: bump dependencies

--- scripts/new-package.mjs
+++ scripts/new-package.mjs
@@ -36,13 +36,14 @@ const pkgJson = {
 	license: 'MIT',
 	sideEffects: false,
 	type: 'module',
-	main: './dist/index.js',
+	main: './dist/index.cjs',
 	module: './dist/index.js',
 	types: './dist/index.d.ts',
 	exports: {
 		'.': {
 			types: './dist/index.d.ts',
 			import: './dist/index.js',
+			require: './dist/index.cjs',
 		},
 	},
 	files: ['dist/**', 'README.md', 'LICENSE'],
